b24fbd4b76b85d4fc68326d4876a9fe7295b2ead
khiminwu <wu.khimin@gmail.com>
2025-04-18 09:28:17 +0700

    update cors

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from app.agent import create_agent
 from app.models import QueryRequest
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify: ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/ask")
 async def ask_agent(request: QueryRequest):
