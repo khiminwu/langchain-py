92cab35400f8b0fae7d9eff663fa47c93bd793c7
khiminwu <wu.khimin@gmail.com>
2025-04-21 15:21:18 +0700

    update

--- app/llm.py
+++ app/llm.py
@@ -6,10 +6,14 @@ from langchain_google_vertexai import VertexAI
 from google.cloud import aiplatform
 from google.oauth2 import service_account
 from langchain.schema import SystemMessage
-
+from google.oauth2 import service_account
+import json
 load_dotenv()
 
-
+creds_json =json.loads(os.getenv("GOOGLE_SERVICE_ACCOUNT"))
+print(creds_json)
+credentials = service_account.Credentials.from_service_account_info(creds_json)
+# credentials = service_account.Credentials.from_service_account_file("./app/google.json")
 def llm():
     
     # open router
@@ -21,23 +25,24 @@ def llm():
     # )
 
     # together
-    return ChatOpenAI(
-        model="mistralai/Mixtral-8x7B-Instruct-v0.1", 
-        # model="meta-llama/Llama-4-Scout-17B-16E-Instruct", # model dari Together ai
-        max_tokens=1024,
-        openai_api_base="https://api.together.xyz/v1",
-        openai_api_key="12180e976ee41b0a7777732964c45a6768a4d7678c45417df37e6ba042a1bf48",
-        temperature=0.7,
-    )
-
-    # return VertexAI(
-    #     model_name="gemma-3-12b-it",
-    #     project="devops-440402",
-    #     location="us-central1",
+    # return ChatOpenAI(
+    #     model="mistralai/Mixtral-8x7B-Instruct-v0.1", 
+    #     # model="meta-llama/Llama-4-Scout-17B-16E-Instruct", # model dari Together ai
+    #     max_tokens=1024,
+    #     openai_api_base="https://api.together.xyz/v1",
+    #     openai_api_key="12180e976ee41b0a7777732964c45a6768a4d7678c45417df37e6ba042a1bf48",
     #     temperature=0.7,
-    #     credentials=credentials,
-    #     verbose=True,
-    #     max_output_tokens=1024  # ✅ This is the correct parameter
     # )
+
+    return VertexAI(
+        model_name="gemini-2.0-flash-lite-001",
+        temperature=0.2,
+        max_output_tokens=100,
+        top_k=40,
+        top_p=0.95,
+        credentials=credentials,
+        project="devops-440402",  # this must match your credentials' project
+        location="us-central1",
+    )
     
 
